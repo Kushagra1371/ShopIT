49179500015689796e39aeffb233d38792369910
Kushagra1371 <116947438+Kushagra1371@users.noreply.github.com>
2025-05-02 02:47:21 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,8 +19,7 @@ const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 
-app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the request
-app.use(cookieParser());
+app.use(express.json({ limit: "10mb" }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
